3f8aa9598de33433ce3dbc5797e0526f5bcd89f9
Banditolabs <Joshuagosst@gmail.com>
2025-12-04 08:02:05 -0800

    reworking db Dir structure

--- backend/db.ts
+++ backend/db.ts
@@ -1,1 +1,20 @@
-const Database = require('better-sqlite3');
+import Database from 'better-sqlite3';
+import fs from 'node:fs';
+import path from 'node:path';
+import { app, Data } from 'electron';
+
+class AppDatabase {
+  public db: Database.Database;
+
+  constructor() {
+    const dbPath = path.join(app.getPath('userData'), 'transactions.sqlite');
+    this.db = new Database(dbPath)
+    this.db.pragma('journal_mode = WAL');
+
+  }
+  initDatabase() {
+    this.db.exec(`
+
+      `)
+  }
+}
